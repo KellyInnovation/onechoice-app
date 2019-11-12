cfc38710371ccf58ba6687a7c541b48559acbd3a
billbunkum <billbunkum@billbunkums-MacBook-Air.local>
2019-11-11 23:53:42 -0500

    refactor TabBarView into  topTabs

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,9 +1,10 @@
-import 'package:one_choice/sheets/bottom_sheet.dart';
+// import 'package:one_choice/sheets/bottom_sheet.dart';
 import 'package:flutter/material.dart';
 
 import '../bottomNavBar.dart';
-import 'package:one_choice/sheets/bottom_sheet.dart';
-import 'package:one_choice/widgets/onechoicedata_agent.dart';
+import '../topTabs.dart';
+// import 'package:one_choice/sheets/bottom_sheet.dart';
+// import 'package:one_choice/widgets/onechoicedata_agent.dart';
 
 const TITLE = "One Choice";
 
@@ -14,69 +15,16 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var ht = 80.0;
+    // var ht = 80.0;
     return
       DefaultTabController(
         length: 4,
         child:
         Scaffold(
-          body:
-          TabBarView(
-            children: [
-              Container(
-                child:Padding(
-                  padding: const EdgeInsets.only(top:40.0),
-                  child: Text("Market"),
-                ),
-              ),
-              Container(
-                child:Padding(
-                  padding: const EdgeInsets.only(top:40.0),
-                  child: Text("Chat"),
-                ),
-              ),
-              Container(
-                child:Padding(
-                  padding: const EdgeInsets.only(top:40.0),
-                  child: Text("Home"),
-                ),
-              ),
-              Container(child:Padding(
-                padding: const EdgeInsets.only(top:40.0),
-                child: Text("Profile"),
-              ),
-              ),
-            ],
-          ),
-// CONTENT
+          body: 
+            TopTabs(),
           bottomNavigationBar: BottomNavBar(),
-          // bottomNavigationBar: BottomAppBar(
-          //   elevation: 0,
-          //   color: Color(0xff344955),
-          //   child: Container(
-          //     padding: EdgeInsets.symmetric(horizontal: 10.0),
-          //     height: ht,
-          //     child: Column(
-          //       children: <Widget>[
-          //         MyBottomSheet(context),
-          //         TabBar(
-          //           tabs: [
-          //             Tab(icon: Icon(Icons.shop), text: "Market",),
-          //             Tab(icon: Icon(Icons.chat_bubble), text: "Chat",),
-          //             Tab(icon: Icon(Icons.home), text: "Home",),
-          //             Tab(icon: Icon(Icons.person), text: "Profile",)
-          //           ],
-          //         ),
-          //       ],
-          //     ),
-          //   ),
-          // ),
         ),
       );
   }
 }
-
-
-/*
-
- */
---
+++ lib/topTabs.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class TopTabs extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+    return Container(
+      child:
+      TabBarView(
+        children: [
+          Container(
+            child:Padding(
+              padding: const EdgeInsets.only(top:40.0),
+              child: Text("Market"),
+            ),
+          ),
+          Container(
+            child:Padding(
+              padding: const EdgeInsets.only(top:40.0),
+              child: Text("Chat"),
+            ),
+          ),
+          Container(
+            child:Padding(
+              padding: const EdgeInsets.only(top:40.0),
+              child: Text("Home"),
+            ),
+          ),
+          Container(child:Padding(
+            padding: const EdgeInsets.only(top:40.0),
+            child: Text("Profile"),
+          ),
+          ),
+        ],
+      ),
+    );
+  }
+}
