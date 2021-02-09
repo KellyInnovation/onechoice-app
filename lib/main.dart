f82e9a9cd871530be2630b39b6909b3adf331ec7
Bill Bunkum <billbunkum@Billy-Boy-Uno.local>
2021-02-09 18:09:41 -0500

    add theme date

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'One Choice Initiative',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -20,13 +20,22 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        bottomAppBarColor: Color.fromRGBO(37, 185, 170, 1),
+        brightness: Brightness.light,
+        fontFamily: 'NotoSansJP',
+        primaryColor: Color.fromRGBO(37, 185, 170, 1),
+        secondaryHeaderColor: Color.fromRGBO(47, 38, 119, 1), // dark purple
+        textTheme: TextTheme(
+          bodyText1: TextStyle(
+            fontSize: 18.0,
+          ),
+        ),
         // This makes the visual density adapt to the platform that you run
         // the app on. For desktop platforms, the controls will be smaller and
         // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'no routes yet'),
     );
   }
 }
